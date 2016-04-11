9c13fdc185a2c3c86a018ecaeaf00b276e5185a2
Sergey Garkusha <sagarkusha@rambler.ru>
2016-04-11 06:32:31 +0000

    Some new changes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,57 +1,26 @@
 'use strict';
 
-var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
-
-module.exports = function (app, passport) {
-
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
-
-	var clickHandler = new ClickHandler();
-
+var moment = require('moment');
+module.exports = function (app) {
 	app.route('/')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/index.html');
-		});
-
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
-		});
-
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
-
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
-
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
+	   .get(function(req, res) {
+	   	  res.sendFile(process.cwd() + '/public/index.html');
+	   });
+	app.route('/:time')
+	   .get(function(req, res) {
+	     var out = {unix: "", natural: ""};
+         var d = new Date(Date.parse(req.params.time));
+         console.log(moment(new Date(req.params.time)).isValid());
+         if (moment(new Date(req.params.time)).isValid()) {
+           out.natural = moment(Date.parse(req.params.time)).format('MMM DD, YYYY');
+           out.unix = Date.parse(req.params.time) / 1000;
+         }else if (Number.isInteger(+req.params.time)){
+           out.natural = moment(req.params.time * 1000).format('MMM DD, YYYY');
+           out.unix = req.params.time;
+         }else{
+           out.unix = 'null';
+           out.natural = 'null';
+         }
+         res.end(JSON.stringify(out));
+	   });
 };
